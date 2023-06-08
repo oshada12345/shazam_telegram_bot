d2ec52c9fedfb9122211c10a4b650105c7ce536d
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-08 20:09:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API = ''
+API = '21204722'
 MUSIC_URL = 'https://mp3zen.net/uploads/files/metan-bashnya-klinit.mp3'
-BOT_TOKEN = ''
+BOT_TOKEN = '5782499781:AAGJRQaLbtSDd6yGb1dPEfAz6h1SFzqrhnU'
 
 
 # True - это если тест версия
